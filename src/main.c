53c8cb321dadcd70a2755c0d558a5764676efa2f
Tavares <lleitetavares@gmail.com>
2020-03-16 21:33:05 -0300

    Comentado/Funcionando 100%

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
 
-/* Data de submissao:
+/* Data de submissao: 13/03/2020
  * Nome: Lucas Leite Tavares  
  * RA: 156377
  */
 
 #include <stdio.h>
+
+/* Setamos o tamos do vetor buffer*/
 const int tam_buffer=100;
 
+/*Chamada da função que verifica a string e inverte as letras*/
 void Inverter(char *buffer){
     char contador = 1, a, b, c = 0, d, aux;
-
+    /* O laço abaixo verifica nosso vetor e quando encontra um \0, uma string, incrementa d,
+    quando tivermos um espaço ou o final da string, setamos o contador.*/
     for(d = 0; buffer[d] != '\0'; d++)
         contador += (buffer[d] == ' '|| buffer[d] == '\n') ? 1 : 0;
-
+    /* Quando o contador é setado, temos um laço que tem a função de alterar a posição das letras
+    ou seja, posição no buffer, assim invertendo as palavras.*/
     do{
         if(contador == 1){
             for(b = c, --d; d > b; d--, b++)
@@ -25,7 +30,8 @@ void Inverter(char *buffer){
         }
     }while(--contador);
 }
-
+/* Nossa main lê a string, coloca no vetor buffer, chmama a função que inverte,
+e por final print o vetor buffer com a frase invertida*/
 int main(){
   char buffer[tam_buffer];
   fgets(buffer, tam_buffer, stdin);
